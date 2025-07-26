ef65d8a2f9cfb336c5b61ceb2065941d4d94dc33
momokan116 <karen1106m@gmail.com>
2025-07-26 15:08:17 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// jQueryの読み込みが終わったら実行
+$(function () {
+ $('.hamburger').click(function(){
+  $(this).toggleClass('active');
+  $('.menu').toggleClass('open');
+  $('overlay').toggleClass('open');
+ });
+ $('.overlay').click(function(){
+  $('.hamburger').removeClass('active');
+  $('.menu').removeClass('open');
+  $(this).removeClass('open');
+ });
+});
